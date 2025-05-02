ad25a949cd53ab99c19e86b0c77e28515e787167
Grazielle <grazielle.anaia@gmail.com>
2025-05-02 11:34:12 -0400

    initial commit

---
+++ src/test/java/com/grazielleanaia/bff_schedulingtask_api/BffSchedulingtaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.bff_schedulingtask_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffSchedulingtaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
